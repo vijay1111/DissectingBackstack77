c5d38be3ccceb6d74ab4479d53586015a7c24e79
vijay Nailwal <16744672+vijay1111@users.noreply.github.com>
2018-11-28 21:27:42 +0530

    first commit of view initialisation

--- app/src/main/java/com/example/vijay/dissectingbackstack7/MainActivity.java
+++ app/src/main/java/com/example/vijay/dissectingbackstack7/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.vijay.dissectingbackstack7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textview);
+
+
     }
 }
